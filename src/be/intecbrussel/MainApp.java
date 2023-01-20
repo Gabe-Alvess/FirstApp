26c9364d0125ccaf00855392de6e5dfb120fa199
Biel-Alves <gabriel_vieira2018@hotmail.com>
2023-01-20 09:40:46 +0100

    done

---
+++ src/be/intecbrussel/MainApp.java
@@ -0,0 +1,9 @@
+package be.intecbrussel;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello: Gabriel");
+
+        System.out.println("Hello world");
+    }
+}
